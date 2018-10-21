1c6be6bdc131cca2a026cdb5b339d3d88012f275
Ian Stanfield <istanfield09@gmail.com>
2018-10-20 17:25:58 -0700

    Adds scaffolding for tweeter project

---
+++ src/corpus.py
@@ -0,0 +1,17 @@
+import os
+
+import twitter
+
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET= os.environ.get('CONSUMER_SECRET')
+ACCESS_TOKEN_KEY = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+
+api = twitter.Api(
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    access_token_key=ACCESS_TOKEN_KEY,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+
+
